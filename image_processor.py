6891a458215839a3c0e60c91d85bc0a1ecc3ee39
Jessica Loo <jessicaloohw@gmail.com>
2018-04-14 22:39:43 -0400

    add skeleton code

---
+++ image_processor.py
@@ -0,0 +1,26 @@
+import numpy as np
+from skimage import io
+
+def run_image_processing(filepaths, command):
+    pass
+
+def histogram_equalization():
+    pass
+
+def contrast_stretching():
+    pass
+
+def log_compression():
+    pass
+
+def reverse_video():
+    pass
+
+def canny_edge_detection():
+    pass
+
+def convert_to_grayscale():
+    pass
+
+def skeletonize():
+    pass
---
+++ test_image_processor.py
@@ -0,0 +1,27 @@
+def test_histogram_equalization():
+    from image_processor import histogram_equalization
+    pass
+
+def test_contrast_stretching():
+    from image_processor import contrast_stretching
+    pass
+
+def test_log_compression():
+    from image_processor import log_compression
+    pass
+
+def test_reverse_video():
+    from image_processor import reverse_video
+    pass
+
+def test_canny_edge_detection():
+    from image_processor import canny_edge_detection
+    pass
+
+def test_convert_to_grayscale():
+    from image_processor import convert_to_grayscale
+    pass
+
+def test_skeletonize():
+    from image_processor import skeletonize
+    pass
