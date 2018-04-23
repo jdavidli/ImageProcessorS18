0273a3ca65972ff140b7983117262e78b27f5f7e
David <jdl62@duke.edu>
2018-04-23 12:02:41 -0400

    tests uploading images using post request

--- imgproc/src/upload.js
+++ imgproc/src/upload.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Button from 'material-ui/Button';
-import Menu, { MenuItem } from 'material-ui/Menu';
 import axios from 'axios';
 import Dropzone from 'react-dropzone'
 
 class SimpleMenu extends React.Component {
   state = {
-    "data": [],
-    "serverResponse": "",
-    currentImageString: '',
     files: []
   };
 
@@ -22,6 +18,20 @@ class SimpleMenu extends React.Component {
         reader.readAsDataURL(file);
         reader.onloadend = () => {
             console.log(reader.result)
+            const formData = new FormData();
+            formData.append("file", reader.result);
+            formData.append("upload_preset", "ex6elkh6"); // Replace the preset name with your own
+            formData.append("api_key", "436934996138467"); // Replace API key with your own Cloudinary key
+            formData.append("timestamp", (Date.now() / 1000) | 0);
+
+            // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+            return axios.post("https://api.cloudinary.com/v1_1/dikxvn5xi/image/upload", formData, {
+              headers: { "X-Requested-With": "XMLHttpRequest" },
+            }).then(response => {
+              const data = response.data;
+              const fileURL = data.secure_url // You should store this URL for future references in your app
+              console.log(data);
+            })
             // do whatever you want with the file content
         };
         reader.onabort = () => console.log('file reading was aborted');
@@ -31,14 +41,12 @@ class SimpleMenu extends React.Component {
     console.log('files')
   }
 
-
   render() {
-    const { anchorEl } = this.state;
     const dropzoneStyle = {
     width  : "100%",
     height : "100%",
     border : "0px solid black"
-};
+    };
 
     return (
       <div>
