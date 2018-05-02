aeb152813d10ad6a3a04e339cc66b53aaf8631d0
Jessica Loo <jessicaloohw@gmail.com>
2018-05-02 02:23:40 -1000

    fix pep8

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'LaserEyeImageProcessor.tex', 'LaserEye Image Processor Documentation',
+    (master_doc, 'LaserEyeImageProcessor.tex',
+     'LaserEye Image Processor Documentation',
      'Kristen Hagan, David Li, Jessica Loo', 'manual'),
 ]
 
@@ -140,7 +141,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'lasereyeimageprocessor', 'LaserEye Image Processor Documentation',
+    (master_doc, 'lasereyeimageprocessor',
+     'LaserEye Image Processor Documentation',
      [author], 1)
 ]
 
@@ -151,7 +153,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'LaserEyeImageProcessor', 'LaserEye Image Processor Documentation',
+    (master_doc, 'LaserEyeImageProcessor',
+     'LaserEye Image Processor Documentation',
      author, 'LaserEyeImageProcessor', 'One line description of project.',
      'Miscellaneous'),
 ]
