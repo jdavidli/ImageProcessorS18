8af33b05875fdf58b578d299aa15742399d17efe
David <jdl62@duke.edu>
2018-05-03 12:59:52 -0400

    dialog moved into class

--- imgproc/src/SimpleDialog.js
+++ imgproc/src/SimpleDialog.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
-import Avatar from 'material-ui/Avatar';
-import List, { ListItem, ListItemAvatar, ListItemText } from 'material-ui/List';
 import Dialog, { DialogTitle } from 'material-ui/Dialog';
-import PersonIcon from '@material-ui/icons/Person';
-import AddIcon from '@material-ui/icons/Add';
-import Typography from 'material-ui/Typography';
-import blue from 'material-ui/colors/blue';
-
-const emails = ['username@gmail.com', 'user02@gmail.com'];
-const styles = {
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-};
 
 class SimpleDialog extends React.Component {
   handleClose = () => {
@@ -24,14 +9,13 @@ class SimpleDialog extends React.Component {
   };
 
   render() {
-    const { classes, onClose, selectedValue, ...other } = this.props;
+    const { classes, onClose, ...other } = this.props;
 
     return (
       <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" {...other}>
         <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
         <div>
-        this.props.pass
-          Hello
+        Hello
         </div>
       </Dialog>
     );
@@ -40,8 +24,7 @@ class SimpleDialog extends React.Component {
 
 SimpleDialog.propTypes = {
   classes: PropTypes.object.isRequired,
-  onClose: PropTypes.func,
-  selectedValue: PropTypes.string,
+  onClose: PropTypes.func
 };
 
-export default withStyles(styles)(SimpleDialog)
+export default SimpleDialog
